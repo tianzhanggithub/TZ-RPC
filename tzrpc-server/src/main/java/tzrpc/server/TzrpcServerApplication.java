0521f50981e957ef0145fc71e29cd818977dc4fa
songjunbo <3096798109@qq.com>
2024-07-13 17:31:08 +0800

    初始化项目结构

---
+++ tzrpc-server/src/main/java/tzrpc/server/TzrpcServerApplication.java
@@ -0,0 +1,13 @@
+package tzrpc.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TzrpcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TzrpcServerApplication.class, args);
+    }
+
+}
